413fb793f33e4d3d7367810a1374e1a7ff41d30f
navitd_cat <dzmitry.navitski@cat.com>
2026-02-13 19:04:48 +0000

    =updates

--- src/products/prod_Tw7UBkg0EBa9A0.ts
+++ src/products/prod_Tw7UBkg0EBa9A0.ts
@@ -1,4 +1,30 @@
+import {createInviteLink} from "../utils/utils";
+import {sendEmail} from "../utils/email";
+
 export const handleProduct = async (productId: string, customerEmail: string) => {
-  console.log(`[prod_Tw7UBkg0EBa9A0] Processing for ${customerEmail}`);
-  // Product-specific logic here
+
+  // UGC Lab
+  console.log("Практикум по съемке UGC видео для " + customerEmail);
+
+  const body = `
+Благодарим за оплату!
+Приглашаем вас в «Практикум по съемке UGC видео»
+
+Ссылки, контакты и мои личные инструкции уже ждут вас в закрытой Telegram группе.
+
+Обязательно сразу подпишитесь и не теряйте этот чат!
+По этой ссылке вы можете войти только ОДИН РАЗ.
+Не переживайте, возможно небольшое ожидание, но вас обязательно добавят в течение суток.
+
+Заходите сюда ⬇️ 
+
+[TG_LINK]
+
+По техническим вопросам можно писать на email <a href="mailto:svethappy3@gmail.com">svethappy3@gmail.com</a>
+`
+  // create personalized TG link
+  const tgLink = await createInviteLink( "-1003861932078",customerEmail);
+  const personalizedBody = body.replace('[TG_LINK]', tgLink);
+
+  await sendEmail('Svethappy <svethappy3@gmail.com>', customerEmail, 'Практикум по съемке UGC видео"', personalizedBody);
 };
--- src/products/prod_TyLJEeevWeA88V.ts
+++ src/products/prod_TyLJEeevWeA88V.ts
@@ -4,35 +4,28 @@ import {createInviteLink} from "../utils/utils";
 // TEST ENV PROD
 
 export const handleProduct = async (productId: string, customerEmail: string) => {
-// Photo video svet
-  console.log("Курс по созданию качественного контента и развитию соц сетей для " + customerEmail);
+  // UGC Lab
+  console.log("Практикум по съемке UGC видео для " + customerEmail);
 
   const body = `
 Благодарим за оплату!
-Приглашаем вас в проект Photo&Video Svet
+Приглашаем вас в «Практикум по съемке UGC видео»
 
-Все видеоуроки, комплект из фото и видео пресетов, а также конспекты и гайды, инструкции уже ждут вас в закрытой Telegram группе!
+Ссылки, контакты и мои личные инструкции уже ждут вас в закрытой Telegram группе.
 
-Чтобы получить доступ к приобретенным материалам вам обязательно нужно завершить регистрацию⏬⏬⏬
-
-Для этого перейдите по кнопке ниже и подключите Telegram
-Обязательно сразу подпишитесь и не теряйте этот чат!!!
+Обязательно сразу подпишитесь и не теряйте этот чат!
 По этой ссылке вы можете войти только ОДИН РАЗ.
+Не переживайте, возможно небольшое ожидание, но вас обязательно добавят в течение суток.
 
-<a href="[TG_LINK]">
-<button style="background-color: #00adef;color: whitesmoke; padding-bottom: 15px;border-radius: 6px;">
-Подключить телеграм
-</button>
-</a>
+Заходите сюда ⬇️ 
 
-Все материалы уже в доступе для изучения в Telegram, ДОСТУП НАВСЕГДА.
+[TG_LINK]
 
 По техническим вопросам можно писать на email <a href="mailto:svethappy3@gmail.com">svethappy3@gmail.com</a>
 `
-
   // create personalized TG link
-  const tgLink = await createInviteLink( "-1002667063289",customerEmail);
+  const tgLink = await createInviteLink( "-1003861932078",customerEmail);
   const personalizedBody = body.replace('[TG_LINK]', tgLink);
 
-  await sendEmail('Svethappy <svethappy3@gmail.com>', customerEmail, 'Курс "Video&PhotoSvet"', personalizedBody);
+  await sendEmail('Svethappy <svethappy3@gmail.com>', customerEmail, 'Практикум по съемке UGC видео"', personalizedBody);
 };
