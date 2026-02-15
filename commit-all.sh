46a58d6772236021e70ec3c189cc9948b50fc4e7
Dzmitry Navitski <dzmitry.navitski@fanduel.com>
2026-02-15 14:37:46 +0000

    auto update build info

---
+++ commit-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Prompt for commit message
+echo "Enter commit message:"
+read commit_msg
+
+git add .
+git commit -m "$commit_msg"
+npm run build-info
+git commit -m "update build info"
+git push
+
