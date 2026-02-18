dc3c2469421ecf1bb3313ef3dc987575d4c49c04
navitd_cat <dzmitry.navitski@cat.com>
2026-02-18 02:47:37 +0000

    fix email for extra month

--- src/products/prod_TLoR6Pvip9OLxU.ts
+++ src/products/prod_TLoR6Pvip9OLxU.ts
@@ -46,7 +46,7 @@ function keyFromUserId(userID:string) {
 export const handleProduct = async (productId: string, customerEmail: string) => {
 
 // UGC Pulse
-  console.log("Клуб создателей контента - доступ 1 месяц для " + customerEmail);
+  console.log("Клуб создателей контента - дополнительный месяц " + customerEmail);
 
   const ugcPulseChatId = -1002906638589;
   const ugcPulseId = -1002913124875;
@@ -74,28 +74,13 @@ export const handleProduct = async (productId: string, customerEmail: string) =>
     process.exit(1);
   });
 
+
   const body = `
-Благодарим за оплату!
-Приглашаем вас в клуб «UGC Pulse»
-
-Ссылки, контакты и мои личные инструкции уже ждут вас в закрытой Telegram группе.
-
-Обязательно сразу подпишитесь и не теряйте этот чат!
-По этой ссылке вы можете войти только ОДИН РАЗ.
-Не переживайте, возможно небольшое ожидание, но вас обязательно добавят в течение суток.
-
-Заходите сюда ⬇️ 
-
-https://t.me/+8Jz7SAhhFDNiZTFi
-
-Обязательно присоединяйтесь во второй чат - для вопросов и обратной связи от Светланы лично
-
-https://t.me/+SLp3Vox-kMNjZGVi
-
-Все материалы в доступе месяц для изучения в Telegram. 
-И конечно будут дополняться, читайте правила участия!
-
-По техническим вопросам можно писать на email <a href="mailto:svethappy3@gmail.com">svethappy3@gmail.com</a>
+    Дополнительный месяц подписки в клуб «UGC Pulse» оплачен!
+    
+    По техническим вопросам можно писать на email <a href="mailto:svethappy3@gmail.com">svethappy3@gmail.com</a>
 `
   await sendEmail('Svethappy <svethappy3@gmail.com>', customerEmail, 'UGC Pulse', body);
+
+
 };
