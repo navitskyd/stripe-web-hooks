eda98dd1b8ab9ab29bd0d05347477f2891170bdc
Dima <dnavitski@gmail.com>
2026-02-20 23:30:23 +0000

    hide stripe version

--- src/server/api-promo.js
+++ src/server/api-promo.js
@@ -48,7 +48,7 @@ const setupPromoRoutes = (app) => {
                 ? promoCodes.data[promoCodes.data.length - 1].id : undefined;
           } while (starting_after);
           if (found) {
-            console.log(process.env.STRIPE_SECRET);
+            //console.log(process.env.STRIPE_SECRET);
             return res.status(409).json({message: 'Already exists'});
           }
           // Not found, create a new promo code (or just return 201 for demo)
