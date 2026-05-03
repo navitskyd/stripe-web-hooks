0497ce475d5973e058dbf93a873b7b4e78fa1313
Dzmitry Navitski <dzmitry.navitski@fanduel.com>
2026-05-03 13:49:29 +0100

    move static to gcp-svethappy

--- web/photosvet/html/slider-wrapper.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-  setTimeout(function(){
-    var scriptElement=document.createElement('script');
-    scriptElement.type = 'text/javascript';
-    scriptElement.src = "./slider.js";
-    document.head.appendChild(scriptElement);
-  }, 150);
-};
